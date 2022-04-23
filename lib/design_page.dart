3d6c66fcdf94e7454303f91df8b6bb6be674ff12
SaminYeaser <saminyeaser1@gmail.com>
2022-04-24 01:53:39 +0600

    designing

--- lib/design_page.dart
+++ lib/design_page.dart
@@ -11,7 +11,66 @@ class _DesignedPageState extends State<DesignedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.redAccent
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.closed_caption_outlined,color: Colors.black,),
+                    Text('Restaurant is close now,will be open at 9:00 am',style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12
+                    ),)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(Icons.emoji_food_beverage,size: 36,color: Colors.orange,),
+                          Text(
+                            'eFood',
+                            style:
+                                TextStyle(
+                                    color: Colors.orangeAccent,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold
+                                ),
+                          ),
+                        ],
+                      ),
+                      const Icon(
+                        Icons.notifications,
+                        color: Colors.black,
+                        size: 24,
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20,),
+                  const Text('All Categories', style: TextStyle(
+                    fontSize: 14
+                  ),)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'design_page.dart';
 import 'my_home_page.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const DesignedPage() /*MyHomePage()*/,
     );
   }
 }
