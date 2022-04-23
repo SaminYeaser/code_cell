2b7eb3270d192c16695506cc354e32e81e477488
SaminYeaser <saminyeaser1@gmail.com>
2022-04-24 01:40:06 +0600

    appBar

---
+++ lib/design_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DesignedPage extends StatefulWidget {
+  const DesignedPage({Key? key}) : super(key: key);
+
+  @override
+  _DesignedPageState createState() => _DesignedPageState();
+}
+
+class _DesignedPageState extends State<DesignedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
